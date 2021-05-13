134550f27413ffe262b67e9626336dc2ff7685f5
Petr Gazarov <petrgazarov@gmail.com>
2021-05-12 20:32:53 -0700

    Small change

--- api/api-router.js
+++ api/api-router.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.use(express.json());
 
 router.get("/", (req, res) => {
-  const messageOfTheDay = process.env.MOTD || 'Welcome to my API server!';
+  const messageOfTheDay = process.env.MOTD || 'Welcome to my API server! v2';
 
   res.status(200).json({ messageOfTheDay, api: "up" });
 });
