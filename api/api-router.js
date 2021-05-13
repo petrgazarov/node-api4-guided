48a68e95b37e4c7e55061d6b06bcb60b4ba4b9a1
Petr Gazarov <petrgazarov@gmail.com>
2021-05-12 20:09:39 -0700

    Added MOTD

--- api/api-router.js
+++ api/api-router.js
@@ -7,7 +7,9 @@ const router = express.Router();
 router.use(express.json());
 
 router.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+  const messageOfTheDay = process.env.MOTD || 'Welcome to my API server!';
+
+  res.status(200).json({ messageOfTheDay, api: "up" });
 });
 
 router.get("/shouts", (req, res, next) => {
