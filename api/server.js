faf08b6ab76d9ff86057f6c3135d27406aa54c3a
Petr Gazarov <petrgazarov@gmail.com>
2021-09-08 20:28:27 -0700

    Add logger

--- api/server.js
+++ api/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 const apiRouter = require('./api-router.js');
 
 const server = express();
 
 server.use(helmet());
+
+const morganFormat = process.env.NODE_ENV === 'development' ? 'dev' : 'combined';
+server.use(morgan(morganFormat));
 
 server.use('/api', apiRouter);
 
